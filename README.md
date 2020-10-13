a96d9bedc4d443c6b5dd96df3da6984bc1336de4
Pochobs <pochobs@hotmail.com>
2020-10-12 23:17:07 -0500

    New Repo

---
+++ README.md
@@ -0,0 +1,26 @@
+# E-commerce Back End 
+
+This is the back end for an e-commerce site. It allows you to enter, delete, update and select products, categories, tags and product tags through the server-side APIs.
+
+Products are associated with tags through many-to-many relationship. Products and categories have one-to-many relashionship.
+
+# Technologies used
+
+* Node.js
+* Express.js
+* npm, MySQL2, Sequelize, dotenv
+* MySQL database
+* Insomnia
+
+# Functionality of the application - Video
+
+https://drive.google.com/file/d/11fDNyAmj2zRqzddM2fHlUWQJWdoG6hqJ/view?usp=sharing
+
+# GitHub repository
+
+You can find the repository here: https://github.com/evkonradi/e-commerce.
+
+# Page preview
+
+This is the preview of the initial page:
+<img src="./ApplicationPreview.JPG">
---
+++ routes/api/category-routes.js
@@ -0,0 +1,103 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  Category.findAll({
+    attributes: ['id', 'category_name'],
+    include: [
+      {
+        model: Product,
+        attributes: ['id','product_name','price','stock','category_id']
+      }
+    ]
+  })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'category_name'],
+    include: [
+      {
+        model: Product,
+        attributes: ['id','product_name','price','stock','category_id']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No category found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+  Category.create({
+    category_name: req.body.category_name
+  })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.put('/:id', (req, res) => {
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No category found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No category found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
